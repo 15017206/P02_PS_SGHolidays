50035be6434d8887c8b83f5f3061921b19c989a4
15017206 <15017206@myrp.edu.sg>
2017-04-27 15:23:03 +0800

    update 1

--- app/src/main/java/com/example/a15017206/p02sgholidays/MainActivity.java
+++ app/src/main/java/com/example/a15017206/p02sgholidays/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.a15017206.p02sgholidays;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayAdapter aa;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get listView object from xml
+        listView = (ListView) findViewById(R.id.lvHolidayType);
+
+        //Defined Array Values to show in ListView
+        String[] values = new String[]{"Secular", "Ethnic & Religion"};
+
+        //Define a new aa
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+        listView.setAdapter(adapter);
+
+        //ListView item click listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int itemPosition = position;
+
+                String itemValue = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent(MainActivity.this, Main2Activity.class);
+                i.putExtra("type", itemValue);
+                startActivity(i);
+
+            }
+        });
     }
 }
